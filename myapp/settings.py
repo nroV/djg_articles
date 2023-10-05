be31fbd3aa70206add0a9dafc51e6a2fe4ec7b50
Sovan panhavorn <nightpp19@gmail.com>
2023-10-05 10:06:44 +0700

    hal

--- myapp/settings.py
+++ myapp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = Path(__file__).resolve().parent.parent
 # STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
   
 # Extra places for collectstatic to find static files.
 # SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
